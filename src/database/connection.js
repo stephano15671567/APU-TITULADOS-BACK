97076ecd4bbc4c59084c3a18d95a68ca1fd9086e
Chii1914 <133626251+Chii1914@users.noreply.github.com>
2023-11-16 00:39:18 -0300

    SISTEMA EXCEL FUNCIONANDO OWOWOWOWO

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,20 +1,25 @@
-import mysql from "mysql2";
+import mysql2 from "mysql2";
 import values from "../const/const.js";
 
+/* The `connectionConfig` object is storing the configuration details for connecting to a MySQL
+database. It includes the following properties: */
 const connectionConfig = {
   host: values.HOST,
   user: values.USER,
-  password: "",
+  password: "1914",
   database: values.DATABASE,
 };
+/* The code is creating a connection to a MySQL database using the `mysql2` library. */
 
-const tempConnection = mysql.createConnection({
+const connection = mysql2.createConnection({
   host: connectionConfig.host,
   user: connectionConfig.user,
   password: connectionConfig.password,
 });
 
-tempConnection.query(
+/* The code `connection.query(`CREATE DATABASE IF NOT EXISTS ${connectionConfig.database}`, (error) =>
+{ ... })` is executing a SQL query to create a database if it does not already exist. */
+connection.query(
   `CREATE DATABASE IF NOT EXISTS ${connectionConfig.database}`,
   (error) => {
     if (error) {
@@ -24,12 +29,12 @@ tempConnection.query(
 
     console.log("Base de datos creada o ya existente");
 
+    // Conectar a la base de datos
+    connectionConfig.database = connectionConfig.database;
+
     // Cerrar la conexión temporal
-    tempConnection.end();
+    connection.end();
   }
 );
 
-// Crear la conexión real a la base de datos
-const db = mysql.createConnection(connectionConfig).promise();
-
-export default db;
+export default connectionConfig;
