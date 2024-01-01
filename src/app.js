f45314fd0700b717e5bf0c18fdba5021c5e6fae7
Chii1914 <maximiliano.aguirre@alumnos.uv.cl>
2024-01-01 03:23:40 -0300

    Update CORS origin URL in app.js

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ const corsOptions = {
   credentials: true,
   optionSuccessStatus: 200,
   methods: 'GET, PUT, POST, DELETE',
-  origin: 'http://localhost:3000', 
+  origin: 'https://titulados.administracionpublica-uv.cl/', 
 };
 
 app.set('env', value.NODE_ENV);
