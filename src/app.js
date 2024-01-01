20c78a27663dd2f66301c926103c633de6bbf145
Chii1914 <maximiliano.aguirre@alumnos.uv.cl>
2024-01-01 03:46:04 -0300

    Refactor app.js imports and formatting

--- src/app.js
+++ src/app.js
@@ -1,41 +1,45 @@
-import 'dotenv/config';
-import express from 'express';
-import cors from 'cors';
-import morgan from 'morgan';
-import session from 'express-session';
-import value from './const/const.js';
-import uploadRoutes from './routes/uploadRoutes.js';
-import profesoresRoutes from './routes/profesoresRoutes.js';
-import alumnosRoutes from './routes/alumnosRoutes.js';
-import asignacionesRoutes from './routes/asignacionesRoutes.js';
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import session from "express-session";
+import value from "./const/const.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
+import profesoresRoutes from "./routes/profesoresRoutes.js";
+import alumnosRoutes from "./routes/alumnosRoutes.js";
+import asignacionesRoutes from "./routes/asignacionesRoutes.js";
 
 const app = express();
 
 const corsOptions = {
   credentials: true,
   optionSuccessStatus: 200,
-  methods: 'GET, PUT, POST, DELETE',
-  origin: ['https://titulados.administracionpublica-uv.cl', 'http://localhost:3000'], 
+  methods: "GET, PUT, POST, DELETE",
+  origin: [
+    "https://titulados.administracionpublica-uv.cl",
+    "http://localhost:3000",
+  ],
 };
 
-app.set('env', value.NODE_ENV);
-app.set('port', value.RUN_PORT);
+app.set("env", value.NODE_ENV);
+app.set("port", value.RUN_PORT);
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(cors(corsOptions));
-app.use(express.json({ limit: '500MB' }));
+app.use(express.json({ limit: "500MB" }));
 app.use(express.urlencoded({ extended: true }));
-app.use(session({
-  secret: value.SECRET,
-  resave: false,
-  saveUninitialized: false
-}));
-
+app.use(
+  session({
+    secret: value.SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 //endpoints
-app.use('/upload', uploadRoutes);
-app.use('/api/profesores', profesoresRoutes);
-app.use('/api/alumnos', alumnosRoutes);
-app.use('/api/asignaciones', asignacionesRoutes);
+app.use("/upload", uploadRoutes);
+app.use("/api/profesores", profesoresRoutes);
+app.use("/api/alumnos", alumnosRoutes);
+app.use("/api/asignaciones", asignacionesRoutes);
 
 export default app;
