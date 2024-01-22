f4fae37d32673582252e45533910015128054d0b
Chii1914 <maximiliano.aguirre@alumnos.uv.cl>
2024-01-22 00:09:52 -0300

    Add file upload functionality and handle file size limit

--- src/controllers/archivosController.js
+++ src/controllers/archivosController.js
@@ -1,13 +1,30 @@
 import mysql2 from "mysql2/promise";
 import db from "../database/connection.js";
 import jwt from "jsonwebtoken"; 
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const createConnection = async () => {
   return await mysql2.createConnection(db);
 };
 
 export const subirArchivo = async (req, res) => {
-    const files = req.files.file.name;
-    console.log(files)
-    return res.json({ message: "Archivo subido correctamente", status: 200});
+    let file;
+    //CAMBIAR FILENAME POR RUT REQL DEL MONO QL Q LO MANDA     
+
+    if (!req.files || Object.keys(req.files).length === 0) {
+      return res.status(400).json({ message: "No se ha subido ningún archivo" });
+    }
+    //File es el nombre con el que se manda el archivo desde el front
+    file = req.files.file;
+    let path = './src/public/fichas_tesis' + file.name;
+    file.mv(path, (err) => {
+      if (err) {
+        return res.status(500).json({ message: "No se ha podido subir el archivo" });
+      }
+    });
+    res.status(200).json({ message: "Archivo subido con éxito." });
 }
